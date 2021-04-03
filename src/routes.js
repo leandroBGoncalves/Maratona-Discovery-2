ca3fe77104cb9961ce92200ea365ac48447d9ef6
Leandro Gonçalves <leandrodebritogoncalves@gmail.com>
2021-04-03 13:09:53 -0400

    Habilitando o req.bory e trabalhando nos cards jobs da Iindex

--- src/routes.js
+++ src/routes.js
@@ -18,11 +18,45 @@ const profile = {
     "vacation-per-year": 4
 }
 
+//Armazenando os dados profile
+
+const jobs = [
+    {
+        id: 1,
+        name: "Pizzaria do Leandro",
+        "daily-hours": 2,
+        "total-hours": 12,
+        created_at: Date.now()
+    },
+    {
+        id: 2,
+        name: "OneTwo Projects",
+        "daily-hours": 3,
+        "total-hours": 38,
+        created_at: Date.now()
+    }
+]
+
 
 
 // req, res
-routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/', (req, res) => res.render(views + "index", { jobs }))
 routes.get('/job', (req, res) => res.render(views + "/job"))
+routes.post('/job', (req, res) => {
+  // req.bory{ name: 'leandro', 'daily-hours': '2.5', 'total-hours': '5' }
+
+  const lastId = jobs[jobs.length - 1] ? jobs[jobs.length - 1].id : 1
+    //const lastId = jobs[jobs.length - 1]?.id || 1;
+
+    jobs.push({
+        id: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now()
+    })
+    return res.redirect('/')
+})
 routes.get('/job-edit', (req, res) => res.render(views + "/job-edit"))
 routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
 
--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ server.set('view engine', 'ejs')
 // habilitar arquivos staticos
 server.use(express.static("public"))
 
+//Linha abaixo é para liberar o req.bory
+server.use(express.urlencoded({ extended: true }))
 
 server.use(routes)
 
