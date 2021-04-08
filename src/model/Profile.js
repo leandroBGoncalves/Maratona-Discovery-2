79a5c30291e58cfd1b406983124a50ec1ecf3a70
Leandro Gonçalves <leandrodebritogoncalves@gmail.com>
2021-04-08 00:25:07 -0400

    Instalamos e iniciamos o DataBase e ja interligamos o dashboard ao DataBase

--- src/model/Profile.js
+++ src/model/Profile.js
@@ -1,18 +1,31 @@
-// Adicionando dados ao profile pelo ejs
-let data = {
-    name: "Leandro",
-    avatar: "https://avatars.githubusercontent.com/u/79161130?v=4",
-    "monthly-budget": 3000,
-    "days-per-week": 5,
-    "hours-per-day": 5,
-    "vacation-per-year": 4,
-    "value-hour": 75
-};
+//Importando o Banco de Dados
+
+const Database = require('../db/config')
+
 
 module.exports = {
-    get(){
-        return data;
+    async get(){
+        const db = await Database()
+
+        const data = await db.get(`SELECT * FROM profile`)
+
+
+       await db.close()
+
+       
+  //Normalizando os traços - que no SQl esta _ e vamos passar para -
+        return{
+            name: data.name,
+            avatar: data.avatar,
+            "monthly-budget": data.monthly_budget,
+            "days-per-week": data.days_per_week,
+            "hours-per-day": data.hours_per_day,
+            "vacation-per-year": data.vacation_per_year,
+            "value-hour": data.value_hour
+        };
+
     },
+
     update(newData){
         data = newData;
     },
